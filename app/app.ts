54d88e099a284cfa7fdfc941bf2eca5c277c1e38
unknown <miguel_lovino@hotmail.com>
2022-03-22 15:41:59 -0300

    initial commit

---
+++ app/app.ts
@@ -0,0 +1,21 @@
+let message: string = 'Hello world!';
+console.log(message);
+
+let nombre: string = "miguel";
+
+let mensaje: string = `Mi nombre es ${nombre}.
+ Soy nuevo el TypeScript.`;
+ console.log(mensaje);
+
+ let mensajeclaro: string ="este es un mensaje claro";
+ console.log(mensajeclaro);
+
+ let prueba: string = "probando";
+ console.log(prueba);
+
+let pruebatraspilado: string = "esto se pasa a app.js";
+console.log(pruebatraspilado);
+
+let pruba2 : string = "ahorasiiiiiiiiiiiiiiiiiiiiiiiiiiii"
+console.log(pruba2);
+
---
+++ output/app.js
@@ -0,0 +1,15 @@
+"use strict";
+let message = 'Hello world!';
+console.log(message);
+let nombre = "miguel";
+let mensaje = `Mi nombre es ${nombre}.
+ Soy nuevo el TypeScript.`;
+console.log(mensaje);
+let mensajeclaro = "este es un mensaje claro";
+console.log(mensajeclaro);
+let prueba = "probando";
+console.log(prueba);
+let pruebatraspilado = "esto se pasa a app.js";
+console.log(pruebatraspilado);
+let pruba2 = "ahorasiiiiiiiiiiiiiiiiiiiiiiiiiiii";
+console.log(pruba2);
